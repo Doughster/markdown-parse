d36e55c82c0300ea359ae71907532ec379199bc6
Doughster <jtdo@ucsd.edu>
2022-01-28 15:42:50 -0800

    Code to fix failure-inducing input in test-file3.md

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,11 +13,11 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1) {
-                return null;
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            if(openParen == -1) {
+                return null;
+            }
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
