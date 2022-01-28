3ff18283dbeb4518444691807c4907e3be9c8cf8
Doughster <jtdo@ucsd.edu>
2022-01-28 14:23:21 -0800

    Added a print statement that caused error
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41
    -1
    41

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
@@ -12,12 +13,14 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            System.out.println(nextOpenBracket);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             System.out.println("Current Index: " + currentIndex);
+            System.out.println(currentIndex);
         }
         return toReturn;
     }
